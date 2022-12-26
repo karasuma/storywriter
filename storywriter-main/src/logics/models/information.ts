4303dc05ca7f7c566948f1f514dc10cd623e7c3f
Yakumo Karasuma <yakumo.karasuma@gmail.com>
2022-12-26 14:06:53 +0900

    informationのデフォルトの内容を変更

--- storywriter-main/src/logics/models/information.ts
+++ storywriter-main/src/logics/models/information.ts
@@ -1,6 +1,6 @@
 export class Information {
     public Title = "Storywriter";
-    public Subtitle = "v1.2.0 renewal!";
-    public News = new Array<string>();
+    public Subtitle = "v1.2.0";
+    public News = ["2022/12/25: Version 1.2.0 Released!", "・ UI Renewal"];
     public previousStories = new Array<string>();
 }
